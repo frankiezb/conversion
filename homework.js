949244b008888d6c2530b4ebbb2070fbc2c851d0
frankiezb <150290307+frankiezb@users.noreply.github.com>
2023-12-16 10:47:30 -0600

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,10 @@
+
+
+// Get temperature in Celsius from the user
+let temperatureCelsius = prompt("Enter temperature in Celsius:");
+
+
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+
+// Display the result in the console
+console.log(`${temperatureCelsius} Celsius is equal to ${temperatureFahrenheit} degrees Fahrenheit.`);
